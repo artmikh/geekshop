d59200ab9c99b7ac51567c6da53710921fee9cd5
artmikh <artmikh@yandex.ru>
2021-01-14 22:12:12 +0500

    Lesson_5(bugfix with product)

--- ordersapp/forms.py
+++ ordersapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from ordersapp.models import Order, OrderItem
+from mainapp.models import Product
 
 class OrderForm(forms.ModelForm):
     class Meta :
@@ -19,6 +20,7 @@ class OrderItemForm(forms.ModelForm):
     
     def __init__ (self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields['product'].queryset = Product.get_items()
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
             field.help_text = ''
