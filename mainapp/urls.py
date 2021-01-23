beacaa4f988d22958d6ad4a30149bf0abec83b21
artmikh <artmikh@yandex.ru>
2021-01-23 20:03:45 +0500

    Lesson7(bugfix)

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include
-from django.urls import path
+from django.urls import path, re_path
 import mainapp.views as mainapp
 from django.views.decorators.cache import cache_page
 
