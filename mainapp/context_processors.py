16fd7f51425ea203a76d69516f265903d00074d1
artmikh <artmikh@yandex.ru>
2021-01-23 14:17:51 +0500

    bugfix

--- mainapp/context_processors.py
+++ mainapp/context_processors.py
@@ -5,7 +5,7 @@ def basket(request):
    basket = []
 
    if request.user.is_authenticated:
-       basket = Basket.objects.filter(user=request.user).order_by('product_category').select_related()
+       basket = Basket.objects.filter(user=request.user).select_related()
 
    return {
        'basket': basket,
