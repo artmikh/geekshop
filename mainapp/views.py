4b1b11d67f81a09d2c53b99bf0758c6a93618402
artmikh <artmikh@yandex.ru>
2021-01-23 19:45:25 +0500

    Lesson7(low_level)

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,6 +4,7 @@ import random
 import basketapp
 from basketapp.models import Basket
 from django.conf import settings
+from django.core.cache import cache
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from mainapp.models import Product, ProductCategory
@@ -12,13 +13,74 @@ from django.views.generic.detail import DetailView
 
 # Create your views here.
 
+def get_links_menu():
+    if settings.LOW_CACHE:
+        key = 'links_menu'
+        links_menu = cache.get(key)
+        if links_menu is None:
+            links_menu = ProductCategory.objects.filter(is_active=True)
+            cache.set(key, links_menu)
+        return links_menu
+    else:
+        return ProductCategory.objects.filter(is_active=True)
 
+def get_category(pk):
+    if settings.LOW_CACHE:
+        key = f'category_ {pk}'
+        category = cache.get(key)
+        if category is None:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            cache.set(key, category)
+        return category
+    else:
+        return get_object_or_404(ProductCategory, pk=pk)
+
+def get_products():
+    if settings.LOW_CACHE:
+        key = 'products'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(is_active=True, category__is_active=True ).select_related('category')
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(is_active=True, category__is_active=True ).select_related('category')
+
+def get_product(pk):
+    if settings.LOW_CACHE:
+        key = f'product_{pk}'
+        product = cache.get(key)
+        if product is None:
+            product = get_object_or_404(Product, pk=pk)
+            cache.set(key, product)
+        return product
+    else:
+        return get_object_or_404(Product, pk=pk)
+
+def get_products_orederd_by_price():
+    if settings.LOW_CACHE:
+        key = 'products_orederd_by_price'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(is_active=True , category__is_active=True ).order_by('price')
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(is_active=True , category__is_active=True ).order_by('price')
+
+def get_products_in_category_orederd_by_price(pk):
+    if settings.LOW_CACHE:
+        key = f'products_in_category_orederd_by_price_ {pk}'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(category__pk=pk, is_active=True , category__is_active=True).order_by('price')
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True).order_by('price')
 
 def main(request):
-    basket = []
-    if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user)
-    products = Product.objects.filter(is_active=True , category__is_active=True).select_related('category')[:3] 
+    products = get_products()[:3]
     
     content = {
         'title':'Главная',
@@ -34,7 +96,7 @@ def get_basket(user):
         return []
 
 def get_hot_product():
-    products = Product.objects.all()
+    products = get_products()
     return random.sample(list(products), 1)[0]
 
 def get_same_products(hot_product):
@@ -43,19 +105,15 @@ def get_same_products(hot_product):
 
 def products(request, pk=None):
     title = "продукты"
-    links_menu = ProductCategory.objects.all()
+    links_menu = get_links_menu()
 
-    basket = []
-    if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user)
-    
     if pk:
         if pk == '0':
-            products = Product.objects.all().order_by('price')
+            products = get_products_orederd_by_price()
             category = {'name': 'все'}
         else:
-            category = get_object_or_404(ProductCategory, pk=pk)
-            products = Product.objects.filter(category__pk=pk).order_by('price')
+            category = get_category(pk)
+            products = get_products_in_category_orederd_by_price(pk)
 
         content = {
             'title': title,
@@ -103,31 +161,35 @@ class ProductDetailView(DetailView):
     template_name = 'mainapp/product.html'
 
 def product(request, pk):
-    title = 'продукт'
+    title = 'продукты'
 
     content = {
         'title':title,
-        'links_menu':ProductCategory.objects.all(),
-        'product':get_object_or_404(Product, pk=pk),
+        'links_menu':get_links_menu(),
+        'product':get_product(pk),
         # 'basket': get_basket(request.user),  
     }
 
     return render(request, 'mainapp/product.html', content)
 
 def load_from_json(file_name):
-    with open(os.path.join(JSON_PATH, file_name + '.json'), 'r' ,errors='ignore') as infile:
+    with open(os.path.join(json_path, file_name + '.json'), 'r' ,errors='ignore') as infile:
         return json.load(infile)
 
 def contact(request):
-    basket = []
-    if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user)
-    json_path = os.path.join(settings.BASE_DIR, 'mainapp/json')
-    with open(os.path.join(json_path, 'contact__locations.json'), encoding='UTF-8') as json_file:
-        contacts = json.load(json_file)
+    if settings.LOW_CACHE:
+        key = f'locations'
+        locations = cache.get(key)
+        if locations is None:
+            locations = load_from_json('contact__locations')
+            cache.set(key, locations)
+    else:
+        locations = load_from_json('contact__locations')
+
+    
     content = {
         'title':'контакты',
-        'contacts':contacts,
+        'contacts':locations,
         # 'basket': basket,
     }
     return render(request, 'mainapp/contact.html', content)
