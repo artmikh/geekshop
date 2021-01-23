9f1fcd7da924e124c11d4e97220475ad995a23d4
artmikh <artmikh@yandex.ru>
2021-01-23 20:03:22 +0500

    Lesson7(controller_cache)

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import include
 from django.urls import path
 import mainapp.views as mainapp
+from django.views.decorators.cache import cache_page
 
 app_name = 'mainapp'
 
 urlpatterns = [
     path('', mainapp.products, name='index'),
-    path('category/<int:pk>/', mainapp.products, name='category'),
-    path('category/<int:pk>/page/<int:page>/' , mainapp.products, name= 'page' ),
+    re_path( r'^category/(?P<pk>\d+)/$', cache_page(3600)(mainapp.products)),
+    # path('category/<int:pk>/', mainapp.products, name='category'),
+    # path('category/<int:pk>/page/<int:page>/' , mainapp.products, name='page'),
     # path('product/<int:pk>/', mainapp.product, name='product'),
     path('product/<int:pk>/', mainapp.ProductDetailView.as_view(), name='product'),
     path('basket/', include('basketapp.urls', namespace='basket')),
