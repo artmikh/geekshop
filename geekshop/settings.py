a91cce0f52fc8bc458bf3ab286c70ea7448fd004
artmikh <artmikh@yandex.ru>
2021-01-23 12:56:44 +0500

    bugfix

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     # 'social_django.middleware.SocialAuthExceptionMiddleware'
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
