c34b17a1906d9ea660c03a443098c61f707a19ab
artmikh <artmikh@yandex.ru>
2021-01-26 23:24:08 +0500

    lesson8(#command)

--- mainapp/management/commands/learn_db.py
+++ mainapp/management/commands/learn_db.py
@@ -1,5 +1,5 @@
 
-from django.core.management.base import BaseCommand
+# from django.core.management.base import BaseCommand
 from mainapp.models import ProductCategory, Product
 from ordersapp.models import OrderItem
 from django.db import connection
@@ -7,17 +7,17 @@ from adminapp.views import db_profile_by_type
 from django.db.models import Q, F, When, Case, DecimalField, IntegerField
 from datetime import timedelta
 
-class Command(BaseCommand):
-   def handle(self, *args, **options):
-       test_products = Product.objects.filter(
-           Q(category__name='офис') |
-           Q(category__name='модерн')
-       )
+# class Command(BaseCommand):
+#    def handle(self, *args, **options):
+#        test_products = Product.objects.filter(
+#            Q(category__name='офис') |
+#            Q(category__name='модерн')
+#        )
 
-       print(len(test_products))
-       # print(test_products)
+#        print(len(test_products))
+#        # print(test_products)
 
-       db_profile_by_type('learn db', '', connection.queries)
+#        db_profile_by_type('learn db', '', connection.queries)
 
 ACTION_1 = 1
 ACTION_2 = 2
